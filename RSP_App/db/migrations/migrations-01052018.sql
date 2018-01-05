df7f1198c393b26537b674b4da14f47326e53945
Ashley Amin <ashley.starks@gmail.com>
2018-01-05 16:31:20 -0500

    minor change to order of SQL

--- RSP_App/db/migrations/migrations-01052018.sql
+++ RSP_App/db/migrations/migrations-01052018.sql
@@ -1,16 +1,16 @@
 \c precrisis_checklist
 
+CREATE TABLE IF NOT EXISTS locations (
+  id BIGSERIAL PRIMARY KEY NOT NULL,
+  title TEXT
+);
+
 CREATE TABLE IF NOT EXISTS items (
   id BIGSERIAL PRIMARY KEY NOT NULL,
   title VARCHAR(75),
   description VARCHAR(255),
   location_id INTEGER REFERENCES locations(id),
   user_id INTEGER REFERENCES users(id)
-);
-
-CREATE TABLE IF NOT EXISTS locations (
-  id BIGSERIAL PRIMARY KEY NOT NULL,
-  title TEXT
 );
 
 CREATE TABLE IF NOT EXISTS users (
--- RSP_App/db/seeds/items.sql
+++ RSP_App/db/seeds/items.sql
@@ -1,31 +1,31 @@
 \c precrisis_checklist
 
-INSERT INTO items (title, description, location_id) VALUES
+INSERT INTO items (title, description) VALUES
   (
   'Durable Power of Attorney',
-  'Create document to appoint an Agent to make legal/financial decision for you.',
+  'Create document to appoint an Agent to make legal/financial decision for you.'
   ),
   (
   'Will',
-  'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum justo lacus, eleifend tincidunt odio non, vulputate tincidunt nulla. ',
+  'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum justo lacus, eleifend tincidunt odio non, vulputate tincidunt nulla. '
   ),
   (
   'Advance Directive',
-  'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum justo lacus, eleifend tincidunt odio non, vulputate tincidunt nulla. ',
+  'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum justo lacus, eleifend tincidunt odio non, vulputate tincidunt nulla. '
   ),
   (
   'Life Insurance Policy',
-  'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum justo lacus, eleifend tincidunt odio non, vulputate tincidunt nulla. ',
+  'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum justo lacus, eleifend tincidunt odio non, vulputate tincidunt nulla. '
   ),
   (
   'Health Insurance',
-  'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum justo lacus, eleifend tincidunt odio non, vulputate tincidunt nulla. ',
+  'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum justo lacus, eleifend tincidunt odio non, vulputate tincidunt nulla. '
   ),
   (
   'Digital Asset Manager',
-  'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum justo lacus, eleifend tincidunt odio non, vulputate tincidunt nulla. ',
+  'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum justo lacus, eleifend tincidunt odio non, vulputate tincidunt nulla. '
   ),
   (
   'Social Security (SSI)',
-  'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum justo lacus, eleifend tincidunt odio non, vulputate tincidunt nulla. ',
+  'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum justo lacus, eleifend tincidunt odio non, vulputate tincidunt nulla. '
   );
