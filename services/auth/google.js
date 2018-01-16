89b8b2b0919cbf1b2c6555b7a3750cd40c443928
Ashley Amin <ashley.starks@gmail.com>
2018-01-16 12:27:45 -0500

    fixed callback for heroku

--- services/auth/google.js
+++ services/auth/google.js
@@ -3,34 +3,10 @@ module.exports = {
     'googleAuth' : {
         'clientID'      : '455936992651-vih6k5il0gqokmnkbfdpqfuc5u2k89mu.apps.googleusercontent.com',
         'clientSecret'  : 'QJ23aEcMNuvDEv7zoj3_V55Y',
-        'callbackURL'   : 'http://localhost:7000/auth/google/callback'
+        'callbackURL'   : 'http://readysetplan.herokuapp.com/auth/google/callback'
     }
 
 };
 
 
-
-// const passport = require('passport');
-// const GoogleStrategy = require('passport-google-oauth20').Strategy;
-
-// const init = require('./passport');
-// const User = require('../../models/user');
-// const authHelpers = require('./auth-helpers');
-
-// const options = {};
-
-// init();
-
-// passport.use(new GoogleStrategy({
-//     clientID: '455936992651-vih6k5il0gqokmnkbfdpqfuc5u2k89mu.apps.googleusercontent.com',
-//     clientSecret: 'QJ23aEcMNuvDEv7zoj3_V55Y',
-//     callbackURL: "http://localhost:7000/auth/google/callback"
-//   },
-//   function(accessToken, refreshToken, profile, cb) {
-//     User.findOrCreate({ googleId: profile.id }, function (err, user) {
-//       return cb(err, user);
-//     });
-//   }
-// ));
-
-// module.exports = passport;
+// for localhost 'http://localhost:7000/auth/google/callback'
